b5008d3fece90d3aa320bc6f5b02b8afc1a02dba
Hager <hajerahmmed525@gmail.com>
2026-07-16 11:56:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,6 @@ st.set_page_config(
     page_icon="🛒",
     layout="wide"
 )
-# ==========================================
-# Load CSS
-# ==========================================
-
-with open("assets/style.css") as f:
-    st.markdown(
-        f"<style>{f.read()}</style>",
-        unsafe_allow_html=True
-    )
 
 # ==========================================
 # Load Logo
