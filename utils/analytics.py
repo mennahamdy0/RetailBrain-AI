4e668972e8309daa6ed3612f8e80fd6162a3fe4a
Hager <hajerahmmed525@gmail.com>
2026-07-16 03:22:38 +0300

    Update analytics.py

--- utils/analytics.py
+++ utils/analytics.py
@@ -1,1 +1,33 @@
 
+"""
+Analytics Utilities
+"""
+
+import numpy as np
+
+
+def get_statistics(result):
+
+    """
+    Calculate detection statistics.
+    """
+
+    total_products = len(result.boxes)
+
+    if total_products > 0:
+
+        confidences = result.boxes.conf.cpu().numpy()
+
+        average_confidence = float(np.mean(confidences))
+
+    else:
+
+        average_confidence = 0.0
+
+    return {
+
+        "products": total_products,
+
+        "avg_confidence": average_confidence
+
+    }
